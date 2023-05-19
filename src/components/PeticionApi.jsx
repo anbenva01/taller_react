1c92596167ea089ac019ca11fef85a4c7dc2f7a5
ANDRES <andresb@example.com>
2023-05-18 23:21:54 -0500

    paginacion de las fotos y detaller de los personajes

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -4,17 +4,17 @@ import React from 'react'
 const PeticionApi = () => {
 
   const [personajes, setPersonajes] = React.useState([]);
-  const [paginacion, setPaginacion] = React.useState(1);
+  const [paginacion, setPaginacion] = React.useState(0);
 
   const obtenerPersonajes = async() =>{
     try {
       //const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
       //const res = await fetch("https://hp-api.onrender.com/api/characters");
       //const res = await fetch("https://api.sampleapis.com/simpsons/characters");
-      //const res = await fetch("https://rickandmortyapi.com/api/character/?page=${paginacion}");
+      const res = await fetch(`https://rickandmortyapi.com/api/character/?page=${paginacion} `);
       //const res = await fetch("https://spapi.dev/api/characters");
       //const res = await fetch("https://api.thecatapi.com/v1/images/search");
-      const res = await fetch("https://apisimpsons.fly.dev/api/personajes");
+      //const res = await fetch("https://apisimpsons.fly.dev/api/personajes");
       const {results} = await res.json();
       setPersonajes(results);
       console.log(results);
@@ -24,16 +24,28 @@ const PeticionApi = () => {
     
   }
 
+  const siguiente = async() => {
+    setPaginacion(paginacion+1);
+    obtenerPersonajes();
+  }
+  const anterior = async() => {
+    setPaginacion(paginacion-1);
+    obtenerPersonajes();
+  }
+
   return (
     <div>
-      <h1>PERSONAJES DE HARY POTTER</h1>
+      <h1>PERSONAJES DE RICK AND MORTY</h1>
       <button onClick={obtenerPersonajes}>Traer personajes</button>
-      <button>Anterior</button>
-      <button>Siguiente</button>
+      <button onClick={anterior}>Anterior</button>
+      <button onClick={siguiente}>Siguiente</button>
       {
-        personajes.map(({id, name}) => (
+        personajes.map(({id, name,image,gender}) => (
           <div key={id}>
-            <h4>{name}</h4>
+            <h4>Nombre: {name}</h4>
+            <h4>Género: {gender}</h4>
+            <h4>Imagen: </h4>
+            <img src={image} alt={name}></img>
           </div>
         ))
       }
